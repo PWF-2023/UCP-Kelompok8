43dc5d5ff19079225b8fbc2cfe226518cb38db42
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2023-05-23 19:55:37 +0700

    Edit Category Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,13 +12,50 @@ class CategoryController extends Controller
         return view('category.index');
     }
 
+    public function store(Request $request, Category $category)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $category = Category::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('category.index')->with('success', 'Category created successfully!');
+    }
+
     public function create()
     {
         return view('category.create');
     }
 
-    public function edit()
+    public function edit(Category $category)
     {
-        return view('category.edit');
+        if (auth()->user()->id == $category->user_id) {
+            return view('category.edit');
+        } else {
+            return redirect()->route('category.index')->with('danger', 'You are not authorized to edit this category!');
+        }
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $category->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('category.index')->with('success', 'Category updated successfully!');
+    }
+
+    public function destroy(Category $category)
+    {
+        if (auth()->user()->id == $category->user_id) {
+            $category->delete();
+            return redirect()->route('category.index')->with('success', 'Category deleted successfully!');
+        } else {
+            return redirect()->route('category.index')->with('danger', 'You are not authorized to delete this category!');
+        }
     }
 }
