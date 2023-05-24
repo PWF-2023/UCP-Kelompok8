79b1004f64100681a744943b3f9290b7eb3abd55
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-05-24 20:13:04 +0700

    adding route for category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
@@ -28,6 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('category', CategoryController::class);
 
     Route::resource('todo', TodoController::class)->except(['show']);
 
