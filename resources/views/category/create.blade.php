2f746491c7c25b36115f063d9f2926877758fdfd
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-05-24 21:27:26 +0700

    update index.blade.php and create.blade.php for category

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="post" action="{{ route('todo.store') }}" class="">
+                    <form method="post" action="{{ route('category.store') }}" class="">
                         @csrf
                         @method('post')
                         <div class="mb-6">
@@ -20,7 +20,7 @@
                         </div>
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
-                            <a href="{{ route('todo.index') }}"
+                            <a href="{{ route('category.index') }}"
                                 class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest
                             text-gray-700 uppercase transition duration-150 ease-in-out
                             bg-white border border-gray-300 rounded-md shadow-sm dark:bg-gray-800
